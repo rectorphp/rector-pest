302a28144f7c5a681e7e8b6c36e26d26b1d4b76c
Tomas Votruba <tomas.vot@gmail.com>
2023-02-07 16:55:45 +0000

    kck off test

--- tests/Set/PHPUnitToPest/config/config.php
+++ tests/Set/PHPUnitToPest/config/config.php
@@ -3,7 +3,11 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+<<<<<<< HEAD
 use Rector\Pest\Set\PestSetList;
+=======
+use Rector\PEST\Set\PestSetList;
+>>>>>>> 7a06171 (kck off test)
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->import(PestSetList::PHPUNIT_TO_PEST);
