fcea786e38e3e219080de1bfa946d4a50edf709d
Tomas Votruba <tomas.vot@gmail.com>
2023-02-07 16:53:03 +0000

    set list

---
+++ config/sets/phpunit-to-pest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+/**
+ * @see https://github.com/pestphp/drift
+ * @see https://github.com/mandisma/pest-converter
+ */
+return static function (RectorConfig $rectorConfig): void {
+    // @todo fill this empty space :)
+};
---
+++ src/Set/PestSetList.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Rector\PEST\Set;
+
+use Rector\Set\Contract\SetListInterface;
+
+final class PestSetList implements SetListInterface
+{
+    public const PHPUNIT_TO_PEST = ;
+}
