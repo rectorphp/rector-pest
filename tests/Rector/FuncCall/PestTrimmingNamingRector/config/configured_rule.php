4f71fa2d65b9858729704262e884f86ac0e09a2c
Oliver <oliver@worksome.com>
2023-02-10 08:42:10 +0100

    feat: add name trimming rector

--- tests/Rector/FuncCall/PestTrimmingNamingRector/config/configured_rule.php
+++ tests/Rector/FuncCall/PestTrimmingNamingRector/config/configured_rule.php
@@ -3,12 +3,10 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Pest\Rector\FuncCall\PestItNamingRector;
 use Rector\Pest\Rector\FuncCall\PestTrimmingNamingRector;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->rules([
-        PestItNamingRector::class,
-        PestTrimmingNamingRector::class,
-    ]);
+    $rectorConfig->import(__DIR__ . '/../../../../../config/config.php');
+
+    $rectorConfig->rule(PestTrimmingNamingRector::class);
 };
