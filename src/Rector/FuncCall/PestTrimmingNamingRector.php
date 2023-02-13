feffe6b82287846de6e3737d5fae1208eb970995
Oliver Nybroe <oliver@worksome.com>
2023-02-13 19:11:41 +0100

    Feat: Trim test name (#4)
    Co-authored-by: Tomas Votruba <tomas.vot@gmail.com>

---
+++ src/Rector/FuncCall/PestTrimmingNamingRector.php
@@ -0,0 +1,76 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rector\Pest\Rector\FuncCall;
+
+use Nette\Utils\Strings;
+use PhpParser\Node;
+use PhpParser\Node\Expr\FuncCall;
+use PhpParser\Node\Name;
+use PhpParser\Node\Scalar\String_;
+use Rector\Core\Rector\AbstractRector;
+use Symplify\RuleDocGenerator\Contract\DocumentedRuleInterface;
+use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
+use Symplify\RuleDocGenerator\ValueObject\RuleDefinition;
+
+/**
+ * @see \Rector\Pest\Tests\Rector\FuncCall\PestItNamingRector\PestItNamingRectorTest
+ */
+final class PestTrimmingNamingRector extends AbstractRector implements DocumentedRuleInterface
+{
+    public function getRuleDefinition(): RuleDefinition
+    {
+        return new RuleDefinition('Removes spaces at the start and end of the test name', [
+            new CodeSample(
+                <<<'PHP'
+test('  has spaces')->skip();
+PHP,
+                <<<'PHP'
+test('has spaces')->skip();
+PHP
+            ),
+        ]);
+    }
+
+    public function getNodeTypes(): array
+    {
+        return [FuncCall::class];
+    }
+
+    /**
+     * @param FuncCall $node
+     */
+    public function refactor(Node $node): ?Node
+    {
+        if (! $this->isNames($node, ['test', 'it', 'todo'])) {
+            return null;
+        }
+
+        $args = (array) $node->args;
+        if (count($args) === 0) {
+            return null;
+        }
+
+        $firstArgument = $args[0];
+
+        if (!$firstArgument instanceof Node\Arg) {
+            return null;
+        }
+
+        $value = $firstArgument->value;
+        if (!$value instanceof String_) {
+            return null;
+        }
+
+        $firstArgumentValue = $value->value;
+
+        if (trim($firstArgumentValue) === $firstArgumentValue) {
+            return null;
+        }
+
+        $firstArgument->value = new String_(trim($firstArgumentValue));
+
+        return $node;
+    }
+}
